f46d68b143bee8b5c39d07eada3184e952469556
Giselle <gdgoenaga@gmail.com>
2023-08-01 17:51:10 -0300

    vistas 1 y 2, sin linkeo a vista 2 completado 1/8 17.51

--- bubba-vue/src/views/Paso1.vue
+++ bubba-vue/src/views/Paso1.vue
@@ -1,39 +1,65 @@
 <template>
-  <!-- <div class="collections">
+  <div class="collections">
     <BagCollection
       v-for="collection in collections"
       :key="collection.id"
       :title="collection.title"
+      :subtitle="collection.subtitle"
       :imageUrl="collection.imageUrl"
       @select="selectCollection(collection)"
     />
-  </div> -->
+  </div>
   <h2>Primera vista: colecciones</h2>
   <h2 @click="goToPaso2">Ir a vista Paso2</h2>
 </template>
 
 <script setup>
-// import { ref } from 'vue'
-// import BagCollection from '../components/BagCollection.vue'
+import { ref } from 'vue'
+import BagCollection from '../components/BagCollection.vue'
 import router from '../router/index.js'
 
-// const collections = ref([
-//   { 
-//     id: 1, 
-//     title: 'Mochila Original', 
-//     imageUrl: 'url-de-la-imagen-1',
-//     products: [
-//       { id: 'prod1', name: 'Producto 1', imageUrl: 'url-producto-1' },
-//       { id: 'prod2', name: 'Producto 2', imageUrl: 'url-producto-2' },
-//       { id: 'prod3', name: 'Producto 3', imageUrl: 'url-producto-3' },
-//     ]
-//   },
+const collections = ref([
+  { 
+    id: 1, 
+    title: 'ORIGINALS',
+    subtitle: 'LINE', 
+    imageUrl: '../../public/Originals/Originals3.jpg',
+    products: [
+      { id: 'Originals1', name: 'Producto 1', imageUrl: '../../public/Originals/Originals2.jpg' },
+      { id: 'Originals2', name: 'Producto 2', imageUrl: '../../public/Originals/Originals1.jpg' },
+    ]
+  },
+  { 
+    id: 2, 
+    title: 'VICTORIA',
+    subtitle: 'LINE', 
+    imageUrl: '../../public/Victoria/Victoria2.jpg',
+    products: [
+      { id: 'Victoria1', name: 'Producto 1', imageUrl: 'url-producto-1' },
+      { id: 'Victoria2', name: 'Producto 2', imageUrl: 'url-producto-2' },
+      { id: 'Victoria3', name: 'Producto 3', imageUrl: 'url-producto-3' },
+    ]
+  },
+  { 
+    id: 3, 
+    title: 'FANCY', 
+    subtitle: 'LINE',
+    imageUrl: '../../public/Fancy/Fancy1.jpg',
+    products: [
+      { id: 'Fancy1', name: 'Producto 1', imageUrl: 'url-producto-1' },
+      { id: 'Fancy2', name: 'Producto 2', imageUrl: 'url-producto-2' },
+      { id: 'Fancy3', name: 'Producto 3', imageUrl: 'url-producto-3' },
+    ]
+  },
 
-// ])
+])
 
-// const selectCollection = (collection) => {
-//   router.push({ name: 'producto', params: { collection: JSON.stringify(collection) } })
-// }
+const selectCollection = (collection) => {
+  router.push({ 
+    name: 'producto', 
+    params: { products: collection.products } 
+  })
+}
 
 
 
--- bubba-vue/src/views/Paso2.vue
+++ bubba-vue/src/views/Paso2.vue
@@ -1,10 +1,21 @@
 <template>
     <h2>Segunda vista: cada producto de la colección</h2>
+
+    <div v-for="product in products" :key="product.id">
+        <h3>{{ product.name }}</h3>
+        <img :src="product.imageUrl" :alt="product.name" />
+    </div>
+
     <h2 @click="goToPaso3">Ir a vista Paso3</h2>
 </template>
 
 <script setup>
 import router from '../router/index.js'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+
+const products = route.params.products
 const goToPaso3 = () => {
     router.push({path: '/encuesta'})
 }
