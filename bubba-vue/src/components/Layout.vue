2a401db17caa4e9f787b41da50781e6696d576da
Lucas Dal Molin <lucasfernandezdalmolin@gmail.com>
2023-08-01 21:15:11 -0300

    Layout creado

---
+++ bubba-vue/src/components/Layout.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+      <header>
+        <h1>{{ titulo }}</h1>
+      </header>
+      <main>
+        <slot></slot>
+      </main>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'LayOut',
+    props: {
+      titulo: {
+        type: String,
+        required: true,
+      },
+    },
+  };
+  </script>
+  
+  <style>
+  div {
+    padding: 20px;
+  }
+  
+  header {
+    text-align: center;
+    background-color: #f0f0f0;
+    padding: 10px;
+  }
+  </style>
+  
--- bubba-vue/src/views/Paso3.vue
+++ bubba-vue/src/views/Paso3.vue
@@ -1,3 +1,59 @@
 <template>
-    <h1>Encuestaaaa</h1>
-</template>
+    <Layout :titulo="tituloPagina">
+      <h2>{{ titulo }}</h2>
+      <form @submit.prevent="submitForm">
+        <div>
+          <label for="campo1">Campo 1:</label>
+          <input v-model="campo1" type="text" id="campo1" required />
+        </div>
+  
+        <div>
+          <label for="campo2">Campo 2:</label>
+          <input v-model="campo2" type="text" id="campo2" required />
+        </div>
+  
+        <div>
+          <label for="campo3">Campo 3:</label>
+          <input v-model="campo3" type="text" id="campo3" required />
+        </div>
+  
+        <div>
+          <label>Puntuación:</label>
+          <star-rating v-model="puntuacion" :star-size="25" :increment="0.5" />
+        </div>
+  
+        <button type="submit">Enviar</button>
+      </form>
+    </Layout>
+  </template>
+  
+  <script>
+import Layout from '../components/Layout.vue';
+
+  
+  export default {
+    data() {
+        return {
+            titulo: 'Formulario de Puntuación',
+            campo1: '',
+            campo2: '',
+            campo3: '',
+            puntuacion: 0,
+            tituloPagina: 'Encuesta',
+        };
+    },
+    methods: {
+        submitForm() {
+            const formData = {
+                campo1: this.campo1,
+                campo2: this.campo2,
+                campo3: this.campo3,
+                puntuacion: this.puntuacion,
+            };
+            console.log(formData);
+        },
+    },
+    components: { Layout }
+};
+  </script>
+  
