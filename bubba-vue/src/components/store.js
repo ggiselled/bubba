d193503d7df6f57921b9ab0a61a58a55e7532494
Giselle <gdgoenaga@gmail.com>
2023-08-02 19:05:50 -0300

    FUNCIONANDO ESE 3DDDDDDDD 2/8 19.05

--- bubba-vue/src/components/store.js
+++ bubba-vue/src/components/store.js
@@ -5,30 +5,46 @@ const collections = [
     id: 1, 
     title: 'ORIGINALS',
     subtitle: 'LINE', 
-    imageUrl: '../../public/Originals/Originals3.jpg',
+    imageUrl: 'Originals/Originals3.jpg',
     products: [
-      { id: 'Originals1', name: 'Original1', imageUrl: '../../public/Originals/Originals1.jpg', images: [/* ruta de las imágenes 3D aquí */] },
-      { id: 'Originals2', name: 'Original2', imageUrl: '../../public/Originals/Originals2.jpg', images: [/* ruta de las imágenes 3D aquí */] },
+      { id: 'Originals1', name: 'Original1', imageUrl: 'Originals/Originals1.jpg', images: 
+      [
+        'Totus vistas/Red Leather Bag.I01.2k.png', 
+        'Totus vistas/Red Leather Bag.I02.2k.png',
+        'Totus vistas/Red Leather Bag.I04.2k.png',
+        'Totus vistas/Red Leather Bag.I05.2k.png',
+        'Totus vistas/Red Leather Bag.I07.2k.png',
+        'Totus vistas/Red Leather Bag.I09.2k.png',
+        'Totus vistas/Red Leather Bag.I11.2k.png',
+        'Totus vistas/Red Leather Bag.I13.2k.png',
+        'Totus vistas/Red Leather Bag.I15.2k.png',
+        'Totus vistas/Red Leather Bag.I16.2k.png'
+
+
+
+
+      ] },
+      { id: 'Originals2', name: 'Original2', imageUrl: 'Originals/Originals2.jpg', images: [/* ruta de las imágenes 3D aquí */] },
     ]
   },
   { 
     id: 2, 
     title: 'VICTORIA',
     subtitle: 'LINE', 
-    imageUrl: '../../public/Victoria/Victoria2.jpg',
+    imageUrl: 'Victoria/Victoria2.jpg',
     products: [
-      { id: 'Victoria1', name: 'Producto 1', imageUrl: '../../public/Victoria/Victoria1.jpg', images: [/* ruta de las imágenes 3D aquí */] },
-      { id: 'Victoria2', name: 'Producto 3', imageUrl: '../../public/Victoria/Victoria3.jpg', images: [/* ruta de las imágenes 3D aquí */] },
-      { id: 'Victoria3', name: 'Producto 4', imageUrl: '../../public/Victoria/Victoria4.jpg', images: [/* ruta de las imágenes 3D aquí */] },
+      { id: 'Victoria1', name: 'Producto 1', imageUrl: 'Victoria/Victoria1.jpg', images: [/* ruta de las imágenes 3D aquí */] },
+      { id: 'Victoria2', name: 'Producto 3', imageUrl: 'Victoria/Victoria3.jpg', images: [/* ruta de las imágenes 3D aquí */] },
+      { id: 'Victoria3', name: 'Producto 4', imageUrl: 'Victoria/Victoria4.jpg', images: [/* ruta de las imágenes 3D aquí */] },
     ]
   },
   { 
     id: 3, 
     title: 'FANCY', 
     subtitle: 'LINE',
-    imageUrl: '../../public/Fancy/Fancy1.jpg',
+    imageUrl: 'Fancy/Fancy1.jpg',
     products: [
-      { id: 'Fancy1', name: 'Producto 1', imageUrl: '../../public/Fancy/Fancy2.jpg', images: [/* ruta de las imágenes 3D aquí */] },
+      { id: 'Fancy1', name: 'Producto 1', imageUrl: 'Fancy/Fancy2.jpg', images: [/* ruta de las imágenes 3D aquí */] },
     ]
   },
 ]
