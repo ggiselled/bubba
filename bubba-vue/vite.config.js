17172d72fcdc9b59a55becfe76963b619d72373e
Giselle <gdgoenaga@gmail.com>
2023-08-06 23:08:04 -0300

    FELI: OJALÁ EL CACHÉ FUNCIONE <3<3<3 6/8 23.07

--- bubba-vue/vite.config.js
+++ bubba-vue/vite.config.js
@@ -1,16 +1,35 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        
+      },
+      workbox: {
+        globDirectory: 'public/',
+        globPatterns: ['**/*.{jpg,png}'], // Tipos de archivo que quiero que me guarde
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, //Configuración de peso de los archivos en caché 
+
+      },
+    })
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
-})
+});
+
+
+
+
+
+
+
